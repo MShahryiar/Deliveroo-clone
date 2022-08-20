a2da4c27c3a7606e9252066b1396369053d3af1f
MShahryiar <muhammadshahryiar@yahoo.com>
2022-08-20 16:36:06 +0500

    Fixed Duplicate Error

---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name:"name",
+      type:'string',
+      title:'Restaurant name',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+
+}
