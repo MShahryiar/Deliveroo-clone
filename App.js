4e64404d47b21cac73d9b79b2cfb7b8a28cc13c4
MShahryiar <muhammadshahryiar@yahoo.com>
2022-08-24 07:42:03 +0500

    added Redux

--- App.js
+++ App.js
@@ -3,18 +3,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import RestaurantScreen from './screens/RestaurantScreen';
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-     <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name='Home' component={Home}/>
-          <Stack.Screen name='Restaurant' component={RestaurantScreen}/>
-        </Stack.Navigator>
-      </TailwindProvider>
+    <NavigationContainer>     
+        <TailwindProvider>
+          <Provider store={store}>
+            <Stack.Navigator>
+              <Stack.Screen name='Home' component={Home}/>
+              <Stack.Screen name='Restaurant' component={RestaurantScreen}/>
+            </Stack.Navigator>
+          </Provider>
+          </TailwindProvider>
     </NavigationContainer>
   );
 }
